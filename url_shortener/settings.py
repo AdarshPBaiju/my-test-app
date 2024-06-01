58c440e419b74b969eaf4ddc642f50364c022d54
Adarsh P Baiju <161452962+AdarshPBaiju@users.noreply.github.com>
2024-06-01 18:00:06 +0530

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -88,6 +88,26 @@ DATABASES = {
 }
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'django': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': False,
+    },
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
