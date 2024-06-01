cc6c5fb3921132790b5ac76a82305f802a382fa3
Adarsh P Baiju <161452962+AdarshPBaiju@users.noreply.github.com>
2024-06-01 17:40:17 +0530

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "url"
+    "URL",
+    "whitenoise.runserver_nostatic"
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+django_heroku.settings(locals())
